5c74b8a6cbb6cdb3c93a000dc55960041433cb1d
Adithya Ekanayaka <130180324+AdithyaSean@users.noreply.github.com>
2024-10-08 00:15:15 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAORuVLXV6nq7G-FwRiwKKjveMxU8BXgxQ",
-  authDomain: "test-737fd.firebaseapp.com",
-  projectId: "test-737fd",
-  storageBucket: "test-737fd.appspot.com",
-  messagingSenderId: "37239167445",
-  appId: "1:37239167445:web:ca2bdc61896eded7f9855c",
-  measurementId: "G-4CCKQW5XQ0"
+
 };
 
 const app = initializeApp(firebaseConfig);
